6f21decfe4137b0719613d7cd7d3758637b8c9de
Gregory Savitsky <gregsavvy@pop-os.localdomain>
2021-03-01 23:38:08 +0300

    Adding menu and punctuation filters

--- Responses.py
+++ Responses.py
@@ -1,15 +1,16 @@
-from datetime import datetime
+import string
+import datetime
 
 def responses(input_text):
-    user_message = str(input_text).lower()
+    user_message = str(input_text).lower().translate(str.maketrans('', '', string.punctuation))
 
-    if user_message in ("hello", "hi", "sup", "wassup", "what's up", "hello there!", "hello there"):
+    if user_message in ('hello', 'hi', 'hello there'):
         return "General Kenobi!"
 
-    if user_message in ("who are you", "who are you?"):
+    if user_message.translate(str.maketrans('', '', string.punctuation)) in ('who are you'):
         return "I'm just a happy camper! Rockin' rollin'!"
 
-    if user_message in ("time", "time?"):
+    if user_message.translate(str.maketrans('', '', string.punctuation)) in ('time'):
         now = datetime.now()
         date_time = now.strftime("%d/%m/%y, %H:%M:%S")
 
--- main.py
+++ main.py
@@ -1,38 +1,71 @@
 import Environment as environment
-from telegram.ext import *
+
 import Responses as responses
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Filters, Updater, MessageHandler, CommandHandler, CallbackQueryHandler, CallbackContext
 
 print("Bot Launched!")
 
+############################ Handlers #########################################
+def start(update, context):
+    update.message.reply_text(main_menu_message(),reply_markup=main_menu_keyboard())
 
-def start_command(update, context):
-    update.message.reply_text('Hello there!')
-
-
-def help_command(update, context):
-    update.message.reply_text('What is thy bidding, master?')
+def help(update, context):
+    update.message.reply_text(main_menu_message(),reply_markup=main_menu_keyboard())
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
     response = responses.responses(text)
-
     update.message.reply_text(response)
 
 def error(update, context):
     print(f"Update {update} caused error {context.error}")
 
+# Keyboard button
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    if query.data == 'help_pattern':
+        query.edit_message_text(text='I know a lot of movies, type some quotes from your favorite movies!')
+
+    elif query.data == 'movie_pattern':
+        query.edit_message_text(text='Hmmmm...\n1. The Thing.\n2. Alien\n3. Lord of the Rings\n4. The Matrix\n5. Snatch\n6. Shawshank Redemption\n7. Forrest Gump\n8. The Green Mile\n9. Gladiator\n10. Schindlers List')
+
+    elif query.data == 'contact_pattern':
+        query.edit_message_text(text='@Gregory_Savitsky')
+
+# Keyboard menu
+def main_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Help', callback_data='help_pattern')],
+              [InlineKeyboardButton('Contacts', callback_data='contact_pattern')],
+              [InlineKeyboardButton('Movie recommendations', callback_data='movie_pattern')]]
+  return InlineKeyboardMarkup(keyboard)
+
+# Menu messages
+def main_menu_message():
+  return 'Take your pick:'
+
+############################# Main func #########################################
 def main():
-    updater = Updater(environment.API_KEY, use_context=True)
+    updater = Updater(environment.API_KEY)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
+    dp.add_handler(CallbackQueryHandler(button))
+
     dp.add_error_handler(error)
 
-    updater.start_polling(5)
+    updater.start_polling()
     updater.idle()
 
-main()
+if __name__ == '__main__':
+    main()
