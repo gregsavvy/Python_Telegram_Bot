cd98ff06be5fc22786b7dc26a0edbbdd4efcb95b
Gregory Savitsky <gregsavvy@pop-os.localdomain>
2021-03-02 20:55:36 +0300

    Adding instructions and additional movie quotes

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 This is a custom telegram bot made with python library telegram-python-bot.
+
+It's written with the following dependencies:
+python-telegram-bot==13.3
+
+There are two ways to install and run this bot.
+
+Option 1 - Clean Python interpreter (Linux):
+
+Installation:
+1. $sudo apt-get install python3 python3-pip git-all
+2. Pull git repo into any directory, - $git remote add demo https://github.com/gregsavvy/Telegram_bot_py.git), then $git pull demo
+3. From main directory, - $pip3 install --no-cache-dir -r requirements.txt
+
+Run:
+1. From main directory, - $python3 main.py
+
+Option 2 - Docker (Linux):
+
+Installation:
+1. Install Docker, - $sudo apt-get install docker-ce docker-ce-cli containerd.io
+2. Install Git, - $sudo apt-get install git-all
+3. Pull git repo into any directory, - $git remote add demo https://github.com/gregsavvy/Telegram_bot_py.git), then $git pull demo
+4. From main directory, - $sudo docker build -t telegrambotpy .
+
+Run:
+1. $sudo docker run telegrambotpy
--- Responses.py
+++ Responses.py
@@ -4,16 +4,48 @@ import datetime
 def responses(input_text):
     user_message = str(input_text).lower().translate(str.maketrans('', '', string.punctuation))
 
-    if user_message in ('hello', 'hi', 'hello there'):
-        return "General Kenobi!"
+    for msg in user_message.split():
 
-    if user_message.translate(str.maketrans('', '', string.punctuation)) in ('who are you'):
-        return "I'm just a happy camper! Rockin' rollin'!"
+        if user_message in ('hello', 'hi', 'hello there'):
+            return "General Kenobi!"
 
-    if user_message.translate(str.maketrans('', '', string.punctuation)) in ('time'):
-        now = datetime.now()
-        date_time = now.strftime("%d/%m/%y, %H:%M:%S")
+        if user_message in ('who are you'):
+            return "I'm just a happy camper! Rockin' rollin'!"
 
-        return str(date_time)
+        if user_message in ('are you free'):
+            return "It’s only after we’ve lost everything that we’re free to do anything."
+
+        if msg in ('try'):
+            return "Do, or do not. There is no “‘try'”."
+
+        if msg in ('love'):
+            return "Sometimes I wish I had never met you. Because then I could go to sleep at night not knowing there was someone like you out there."
+
+        if msg in ('strange'):
+            return "I believe whatever doesn’t kill you, simply makes you…stranger."
+
+        if msg in ('bye', 'goodbye'):
+            return "I'll be back."
+
+        if msg in ('freedom'):
+            return "They may take our lives, but they will never take… our FREEDOM!"
+
+        if msg in ('complicated'):
+            return "My momma always said, “Life is like a box of chocolates, you never know what you’re gonna get."
+
+        if msg in ('motivation', 'sad'):
+            return "Don’t ever let somebody tell you you can’t do something, not even me. Alright? You dream, you gotta protect it. People can’t do something themselves, they wanna tell you you can’t do it. If you want something, go get it. Period."
+
+        if msg in ('life'):
+            return "What we do in life echoes in eternity."
+
+        if msg in ('funny'):
+            return "It's just a flesh wound."
+
+        if user_message in ('time'):
+            now = datetime.now()
+            date_time = now.strftime("%d/%m/%y, %H:%M:%S")
+
+            return str(date_time)
 
     return "What?"
