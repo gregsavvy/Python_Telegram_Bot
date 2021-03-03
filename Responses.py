06a5a2bb20f75229cf5e2e4e57e5a236a4f83790
Gregory <73368243+gregsavvy@users.noreply.github.com>
2021-03-03 11:29:24 +0300

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -12,17 +12,8 @@ def responses(input_text):
         if user_message in ('who are you'):
             return "I'm just a happy camper! Rockin' rollin'!"
 
-        if user_message in ('are you free'):
-            return "It’s only after we’ve lost everything that we’re free to do anything."
-
         if msg in ('try'):
             return "Do, or do not. There is no “‘try'”."
-
-        if msg in ('love'):
-            return "Sometimes I wish I had never met you. Because then I could go to sleep at night not knowing there was someone like you out there."
-
-        if msg in ('strange'):
-            return "I believe whatever doesn’t kill you, simply makes you…stranger."
 
         if msg in ('bye', 'goodbye'):
             return "I'll be back."
@@ -30,19 +21,43 @@ def responses(input_text):
         if msg in ('freedom'):
             return "They may take our lives, but they will never take… our FREEDOM!"
 
-        if msg in ('complicated'):
+        if msg in ('life'):
             return "My momma always said, “Life is like a box of chocolates, you never know what you’re gonna get."
 
         if msg in ('motivation', 'sad'):
             return "Don’t ever let somebody tell you you can’t do something, not even me. Alright? You dream, you gotta protect it. People can’t do something themselves, they wanna tell you you can’t do it. If you want something, go get it. Period."
 
-        if msg in ('life'):
-            return "What we do in life echoes in eternity."
+        if msg in ('trust'):
+            return "Someone in this camp ain't what he appears to be."
 
         if msg in ('funny'):
             return "It's just a flesh wound."
+        
+        if msg in ('bot'):
+            return "I am t-800 cyberdyne systems model 101."
+        
+        if msg in ('serious'):
+            return "I am serious. And don't call me Shirley."
+        
+        if user_message in ('fuck you', 'screw you', 'bad bot', 'f u'):
+            return "I'm about to do to you what Limp Bizkit did to music in the late '90s."
+        
+        if msg in ('evil'):
+            return "We get the warhead and we hold the world ransom for…. One million dollars."
+        
+        if msg in ('old'):
+            return "It's not the years, honey. It's the mileage."
+        
+        if msg in ('question', 'asking'):
+            return "You talkin to me?"
+        
+        if msg in ('die'):
+            return "Yippie-ki-yay, motherf—er."
+        
+        if msg in ('war'):
+            return "I love the smell of napalm in the morning."
 
-        if user_message in ('time'):
+        if msg in ('time'):
             now = datetime.now()
             date_time = now.strftime("%d/%m/%y, %H:%M:%S")
 
